b7a22661837c38b0bdecfb88dbb4be10471b7c0a
CutieKitten392 <nobady928@yahoo.co.jp>
2017-02-11 18:19:06 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -246,7 +246,9 @@ function submitIdolSearchForm() {
 		});
 		elm += '</tr>';
 		$.each(result, function(index, val) {
-			return opt.limit != index;
+			if(opt.limit == index) {
+				return false;
+			}
 			var className;
 			switch(val.type) {
 				case 'ｷｭｰﾄ'  : className = 'cute'; break;
